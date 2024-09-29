217d16322affbd14558e4c4ad58e5a2d5dd6e221
Anil Kumar Behera <anilkumar567849@gmail.com>
2024-09-29 17:34:46 +0530

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,143 @@
-import Image from "next/image";
+'use client';
+import React, { useEffect, useState } from 'react'
+import {Rabbit, Turtle} from 'lucide-react';
 
-export default function Home() {
+const gameInit: number[][] = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+function TicTacToe() {
+
+    const [gameBoard, setGameBoard] = useState<number[][]>(gameInit);
+    const [turn, setTurn] = useState<1 | 2>(1);
+    const [click, setClick] = useState<number>(0);
+
+    useEffect(() => {        
+        // check each row 
+        for(let r = 0; r < 3; r++){
+            let item = gameBoard[r][0];
+            if(item == 0) continue;
+            let found = true;
+            for (let c = 0; c < 3; c++) {
+                if(gameBoard[r][c] != item){
+                    found = false;
+                    break;
+                }
+            }
+            if(found){
+                alert(`winner found: ${item}`)
+                setGameBoard(gameInit);
+                setTurn(1);
+            }
+        }
+
+        // check each col         
+        for(let c = 0; c < 3; c++){
+            let item = gameBoard[0][c];
+            if(item == 0) continue;
+            let found = true;
+            for (let r = 0; r < 3; r++) {
+                if(gameBoard[r][c] != item){
+                    found = false;
+                    break;
+                }
+            }            
+            if(found){
+                alert(`winner found: ${item}`)
+                setGameBoard(gameInit);
+                setTurn(1);
+            }
+        }
+
+        // check diagnol 
+        let item = gameBoard[0][0];
+        let found = true;
+        if(item != 0){
+            for(let i = 0; i < 3; i++){
+                if(gameBoard[i][i] != item){
+                    found = false;
+                    break;
+                }
+            }
+            if(found){
+                alert(`winner found: ${item}`)
+                setGameBoard(gameInit);
+                setTurn(1);
+            }
+        }
+
+            
+        item = gameBoard[0][2];
+        found = true;
+        if(item != 0){
+            for(let i = 0; i < 3; i++){
+                if(gameBoard[i][2-i] != item){
+                    found = false;
+                    break;
+                }
+            }
+            
+            if(found){
+                alert(`winner found: ${item}`)
+                setGameBoard(gameInit);
+                setTurn(1);
+            }
+        }
+
+    }, [gameBoard]) 
+
+    useEffect(() => {        
+        if(click == 9){
+            setGameBoard(gameInit);
+            setTurn(1)
+            setClick(0);
+        }
+    }, [click])
+
+
+    const updateGameBoard = (row: number, col: number, value: number) => {
+        setGameBoard((prevBoard) => {
+          const newBoard = prevBoard.map((r, rowIndex) =>
+            r.map((cell, colIndex) => (rowIndex === row && colIndex === col ? value : cell))
+          );
+          return newBoard;
+        });
+    };
+
+    function handleClick(row: number, col: number){
+
+        if(gameBoard[row][col] != 0) return;
+
+        updateGameBoard(row, col, turn);
+
+        setTurn((prev: 1 | 2) => {
+            return prev === 1 ? 2 : 1;
+        });
+        
+        setClick(click+1);
+    }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className='min-h-screen min-w-screen bg-gradient-to-tr from-indigo-600  to-purple-500 flex justify-center items-center'>
+        <div className='h-[30rem] w-[30rem] border-4 grid grid-rows-3'>
+            {
+                gameBoard.map((row: number[], rowInd: number) => {
+                    return <div key={rowInd.toString()} className='grid grid-cols-3'>
+                        { 
+                            row.map((col: number, colInd: number) => {
+                                return <div 
+                                key={colInd.toString()}
+                                className='border-4 flex justify-center items-center'
+                                onClick={() => handleClick(rowInd, colInd)}>
+                                        <h1>{col == 0 ? "" : col == 1 ? <Rabbit /> : <Turtle />}</h1>
+                                        </div>
+                            })
+                        }
+                    </div>
+   
+                })
+            }
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
-  );
+  )
 }
+
+export default TicTacToe
